b744ff99d1ca641c9ff90a18e4e8b637ee2d8460
reyhaneh <reyhanehrm333@gmail.com>
2022-02-28 21:08:33 +0330

    added logger middleware

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next(); // to move on to the next middleware cycle
+};
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 // routes
-
 const bootcamps = require("./routes/bootcamps");
-
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
 // load env variables
 dotenv.config({ path: "./config/config.env" });
 const app = express();
-
+// dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 // mount routes
-
 app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
 app.listen(
