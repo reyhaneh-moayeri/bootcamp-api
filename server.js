580f675b2e46e42ba411190a3a6ef11998d465f6
reyhaneh <reyhanehrm333@gmail.com>
2022-03-01 19:21:35 +0330

    connected mongodb

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const dotenv = require("dotenv");
 const bootcamps = require("./routes/bootcamps");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 // load env variables
 dotenv.config({ path: "./config/config.env" });
+// connect to database
+connectDB();
 const app = express();
 // dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -14,7 +17,13 @@ if (process.env.NODE_ENV === "development") {
 // mount routes
 app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   () => `server running in ${process.env.NODE_ENV} on port ${PORT}`
 );
+
+// handle rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.error(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
